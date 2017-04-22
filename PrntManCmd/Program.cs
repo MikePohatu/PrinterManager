118d82b86994629624032a098211b73190f0b336
Mike Pohatu <mikepohatu@gmail.com>
2017-04-22 21:42:16 +1200

    .

--- PrntManCmd/Program.cs
+++ PrntManCmd/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace core
+namespace PrntManCmd
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
---
+++ core/PrintServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class PrintServer
+    {
+        public string Name { get; set; }
+        public List<PrintQueue> Queues { get; set; }
+
+        public PrintServer()
+        {
+            this.Queues = new List<PrintQueue>();
+        }
+
+        public void AddQueue(PrintQueue queue)
+        {
+            if (queue != null) { this.Queues.Add(queue); }
+        }
+
+        public void RemoveQueue(PrintQueue queue)
+        {
+            if (queue != null) { this.Queues.Remove(queue); }
+        }
+    }
+}
